8fc5b481d1357e0344168ca71ab10466a5aa8227
alexa <alexander.55.fc@gmail.com>
2024-03-08 10:35:31 +0100

    creada la conexion cambiar el nombre de la base de datos

---
+++ ExamenAlexanderFuela1/ExamenAlexanderFuela1/Models/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExamenAlexanderFuela1.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto (DbContextOptions<Contexto> options) : base(options)
+        {
+        }
+    }
+}
